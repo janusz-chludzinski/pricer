0730b60e137ae175cd853de116119c76182f7fd3
janusz-chludzinski <82462674+janusz-chludzinski@users.noreply.github.com>
2022-06-13 20:29:05 +0200

    parsing config

--- api/main.go
+++ api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/janusz-chludzinski/pricer/server"
+
+	// "github.com/janusz-chludzinski/pricer/server"
+	"github.com/janusz-chludzinski/pricer/config"
 )
 
 func main() {
-	server.Start()
+	// server.Start()
+	products := config.ReadConfig("./config.json")
 }

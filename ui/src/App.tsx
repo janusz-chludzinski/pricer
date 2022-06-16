274e59ec9f691417824fa5d591a41f4b28d6fa53
janusz-chludzinski <82462674+janusz-chludzinski@users.noreply.github.com>
2022-06-16 20:20:29 +0200

    added loading spinner

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,11 +4,11 @@ import "./App.css";
 import { ProductTable } from "./components/ProductTable";
 
 function App() {
-
-
-  return <div className="App">
-    <ProductTable />
-  </div>;
+  return (
+    <div className="App">
+      <ProductTable />
+    </div>
+  );
 }
 
 export default App;
